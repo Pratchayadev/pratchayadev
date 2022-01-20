9fc1b2e1961b91f1e7a37ccb0c747df315e871c6
Pratchayadev <Pratchayadev@users.noreply.github.com>
2022-01-20 09:26:56 +0000

    deploy: f46de07c2413f8a434eebf61bc2607ec35b344a0

--- _next/static/Y1e1eBjYiednSZXtifW9Q/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/32-472156d4fd9c501d.js","static/chunks/pages/index-6b5655da9b67eb9a.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
